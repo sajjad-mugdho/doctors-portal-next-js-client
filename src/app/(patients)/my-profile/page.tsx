22b1cd61c99c45a481f75e05315b72e8007e5c1e
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-10-25 18:50:32 +0600

    [Done] project initilization

---
+++ src/app/(patients)/my-profile/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PatientsProfile = () => {
+  return (
+    <div>
+      <h1>this is patients profiles</h1>
+    </div>
+  );
+};
+
+export default PatientsProfile;
